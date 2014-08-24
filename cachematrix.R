aa21afa1916bcd18684c7cb9508335dbbee8425c
Matt Taylor <mattstaylor@hotmail.com>
2014-08-23 22:56:06 -0700

    r code for PA#2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions combine to  calculate the inverse of a matrix.  The matrix and inverse  
+##  are both cached, so that subsequent queries will retrieve the cached inverse
+## instead of recalculating.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a function that caches a matrix and its inverse.  These variables
+##  are stored in the global environment.  The makeCacheMatrix function returns a list with
+## the name of the newly constructed function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {        
+        cacheMatrix <- function(x) {
+            cachedM <<- x  
+            cachedInv <<- solve(x)
+        }
+    ## Return list of the constructed function name
+    list(cacheMatrix=cacheMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve accepts an invertible matrix as its argument, and determines if x should
+## be solved by calculation or if the solution may be retrieved from a cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## These command lines determine if there is a previously cached solution, and if it
+    ## is applicable to x.
+    if(exists("cachedM")) {
+        if(identical(x,cachedM) && !is.null(cachedInv)) {
+            return(cachedInv)
+        }
+    }
+    ## These commands execute the first function, makeCacheMatrix, to solve for x and cache 
+    ## both x and its inverse
+    a <- makeCacheMatrix(x)
+    a$cacheMatrix(x)
+    cachedInv
 }
